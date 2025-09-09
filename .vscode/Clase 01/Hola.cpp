a39758141c32df08c95908e141c64b48e19c71cc
facusteckler86 <facundomsteckler@gmail.com>
2025-09-08 23:16:08 -0300

    primeros pasos con C++

---
+++ .vscode/Clase 01/Hola.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    //Tipos de Variables
+    int miVariable;
+    char miChar;
+    float miNumeroConComa;
+    double tambienConComa; // Es mas preciso con los numeros decimales
+    bool logica;
+    string cadena;
+
+    miVariable = 13;
+    miChar = 'F';
+    miNumeroConComa = 255.45;
+    tambienConComa = 50.50;
+    logica = true;
+    cadena = "Hola Mundo"; 
+
+    // Codigo de salida
+    cout << cadena;
+
+    //Codigo de entrada
+    cin >> miVariable;
+    }
+    
+
+
+}
